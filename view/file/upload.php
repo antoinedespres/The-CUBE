4b9e24056a064df275a287f6b57f50693d960c11
HocusPocusCitrus <62442663+HocusPocusCitrus@users.noreply.github.com>
2020-11-22 12:41:33 +0100

    Create upload.php

---
+++ view/file/upload.php
@@ -0,0 +1,17 @@
+<?php
+
+include("view/menu.php");
+
+echo '
+<form method="POST" action="/upload" enctype="multipart/form-data">
+	<fieldset>
+		<legend>Upload</legend>
+
+		<label for="file">File: </label>
+		<input type="file" name="file" required>
+	</fieldset>
+	<input type="submit" value="Submit">
+</form>';
+
+if (isset($response))
+	echo $message;
