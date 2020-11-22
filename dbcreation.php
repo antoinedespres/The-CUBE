9debcb2eaa977d994b8f212559c9b9f554c69d5d
HocusPocusCitrus <62442663+HocusPocusCitrus@users.noreply.github.com>
2020-11-22 10:47:21 +0100

    Update dbcreation.php

--- dbcreation.php
+++ dbcreation.php
@@ -4,6 +4,5 @@ $query = file_get_contents("createdb.sql");
 $db = new PDO("sqlite:database.sqlite");
 if($db !== null){
     echo "database created";
-    $stmt = $db->prepare($query);
-    $stmt->execute();
+    $db->exec($query);
 }
